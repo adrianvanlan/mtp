f9e7d6d36905dceb6d0bbc1bcd1c1ac0358ecb24
Agustin Toledo <toledoag@gmail.com>
2016-12-04 03:40:24 -0300

    Update

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-<<<<<<< HEAD
-=======
     url(r'^nodos$', views.nodos),
->>>>>>> 8bd67b3ad2b5f1730aeabe6b062e9dd8efb5d4d6
 ]
--- core/views.py
+++ core/views.py
@@ -28,10 +28,17 @@ def nodos(request):
         G.add_vertex(nodo.id)
     arcos = Arco.objects.all()
     for arco in arcos:
-        G.add_edge(arco.nodo_desde.id, arco.nodo_hasta.id, arco.costo)
+        G.add_edge(arco.nodo_desde.id, arco.nodo_hasta.id, arco.tiempo)
     ids_nodos_bien = shortest_path(G, 1, 7)
     nodos_bien = Nodo.objects.filter(id__in=ids_nodos_bien)
+
+    coords = []
+    nnn = list(i for i in nodos_bien)
+    for idx, n in enumerate(nnn[:len(nnn)-1]):
+        c = {"desde": n, "hasta": nnn[idx+1]}
+        coords.append(c)
+
     return render_to_response(
         "mapa.html",
-        {"nodos": nodos_bien}
+        {"nodos": coords}
     )
