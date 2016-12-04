2793bb32ebabb3aa6a42603deb199f7b7a6168e3
Rodrigo <rodri.velazquez90@gmai.com>
2016-12-03 23:48:10 -0300

    agrego perfil

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 TIPOS_NODO = (
     ("L", "Localidad"),
@@ -6,6 +7,21 @@ TIPOS_NODO = (
     ("A", "Aeropuerto"),
     ("S", "Silos"),
     ("P", "Puerto"))
+
+
+TIPOS_PERFIL = (
+    ("P", "Productor"),
+    ("T", "Transportista")
+)
+
+
+class Perfil(models.Model):
+
+    usuario = models.OneToOneField(
+        User,
+        unique=True
+    )
+    tipo = models.CharField(max_length=1, choices=TIPOS_PERFIL)
 
 
 class Nodo(models.Model):
