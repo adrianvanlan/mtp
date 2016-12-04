1a432a28dce3d173a289b86fd77c3f767eb49eb0
Rodrigo <rodri.velazquez90@gmai.com>
2016-12-04 01:06:07 -0300

    index

--- core/views.py
+++ core/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.http import request
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
 
 def index(request):
-	return render_to_response("index.html", {})
+    return render_to_response("index.html", {})
+
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        render_to_response("index.html", {})
+    else:
+        render_to_response("index.html", {})
+        # Return an 'invalid login' error message.
+
+
+
+def logout_view(request):
+    logout(request)
+    # Redirect to a success page.
