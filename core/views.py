8798e8b956990f6a0088c9e36748d906a5c3a515
Rodrigo <rodri.velazquez90@gmai.com>
2016-12-04 11:27:20 -0300

    landing + login + logout

--- core/views.py
+++ core/views.py
@@ -1,20 +1,4 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.http import request
 from django.contrib.auth import authenticate, login
-
-def index(request):
-    return render_to_response("index.html", {})
-
-def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        login(request, user)
-        render_to_response("index.html", {})
-    else:
-        render_to_response("index.html", {})
-        # Return an 'invalid login' error message.
-
-def logout_view(request):
-    logout(request)
+from django.template import RequestContext
